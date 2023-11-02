9ef0346ebb22c7314ab5fb6a9e7426f8538cd24f
marekwadinger <marekwadinger5506@gmail.com>
2023-11-02 13:39:47 +0100

    FIX: mypy

--- core/simulate.py
+++ core/simulate.py
@@ -1,4 +1,6 @@
 """Simulation of the system"""
+from typing import Callable
+
 import numpy as np
 from scipy.integrate import solve_ivp
 
@@ -10,7 +12,7 @@ def gen_sine_u(
         sim_time: int,
         u_min: list,
         u_max: list,
-        ) -> tuple[float, float, float]:
+        ) -> list[float]:
     """Generate the sine waves for the inputs.
 
     Args:
@@ -20,24 +22,25 @@ def gen_sine_u(
     Returns:
        tuple of us with len equal to len(u_min)
     """
-    t = t / (sim_time - 1)  # Normalize t to the range [0, 1]
+    t_ = t / (sim_time - 1)  # Normalize t to the range [0, 1]
 
-    u = tuple()
+    u: list[float] = []
     for u_min_, u_max_ in zip(u_min, u_max):
         # Calculate the values of each sine wave at time t
-        u_ = u_min_ + (u_max_ - u_min_) / 2 * (1 + np.sin(2 * np.pi * t))
-        u += (u_,)
+        u_: float = (u_min_ + (u_max_ - u_min_) / 2
+                     * (1 + np.sin(2 * np.pi * t_)))
+        u.append(u_)
     return u
 
 
 def simulate_open_loop(
-        system: callable,
+        system: Callable,
         sim_time: int = 300
         ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     """Simulation of the system in the open-loop.
 
     Args:
-        system (callable): _description_
+        system (Callable): _description_
         sim_time (optional): Simulation time in seconds [s]
 
     Returns:
@@ -50,14 +53,14 @@ def simulate_open_loop(
     u_min = [5, 0, 400]
     u_max = [25, 100, 800]
     # Initial mass of structural and non-structural dry weight
-    x_ode = (10, 10)
+    x_ode_prev = (10, 10)
 
     for t in range(sim_time):
         tspan = [t, t+TS]
         u = gen_sine_u(t, sim_time, u_min, u_max)
 
-        x_ode = solve_ivp(system, tspan, x_ode, args=(u,), method="RK45")
-        x_ode = x_ode.y[:, -1]
+        x_ode = solve_ivp(system, tspan, x_ode_prev, args=(u,), method="RK45")
+        x_ode_prev = x_ode.y[:, -1]
 
         y_out.append(x_ode)
         t_out.append(t)
@@ -70,13 +73,13 @@ def simulate_open_loop(
 
 
 def simulate_closed_loop(
-        system: callable,
+        system: Callable,
         sim_time: int = 300
-        ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+        ) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     """Simulation of the system in the closed-loop.
 
     Args:
-        system (callable): _description_
+        system (Callable): _description_
         sim_time (optional): Simulation time in seconds [s]
 
     Returns:
@@ -90,19 +93,19 @@ def simulate_closed_loop(
     u_max = 10
     u_min = 0
 
-    x_ode = (10, 10)
-    x_ref = x_ode
+    x_ode_prev = (10, 10)
+    x_ref = x_ode_prev
 
     for t in range(sim_time):
         tspan = [t, t+TS]
         if t == 50:
-            x_ref = [50, 50]
+            x_ref = (50, 50)
 
         k = np.array([1., 1., 1.])
-        u = -k @ (np.array(x_ode) - np.array(x_ref))
+        u = -k @ (np.array(x_ode_prev) - np.array(x_ref))
         u = max(min(u, u_max), u_min)
-        x_ode = solve_ivp(system, tspan, x_ode, args=(u,), method="RK45")
-        x_ode = x_ode.y[:, -1]
+        x_ode = solve_ivp(system, tspan, x_ode_prev, args=(u,), method="RK45")
+        x_ode_prev = x_ode.y[:, -1]
 
         y_out.append(x_ode)
         y_ref.append(x_ref)
