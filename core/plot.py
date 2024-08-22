3cca65e1dacd397850e7700d1602f452d45d1601
Marek Wadinger <marekwadinger5506@gmail.com>
2024-08-22 12:57:26 +0200

    UPDATE: plot styling

--- core/plot.py
+++ core/plot.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Literal, Union
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,12 +6,15 @@ import pandas as pd
 
 plt.rcParams.update(
     {
-        "axes.labelsize": 8,
+        "text.usetex": True,
+        "font.family": "CMU Serif",
+        "font.serif": "CMU Serif",
         "axes.grid": True,
-        "font.size": 8,
-        "legend.fontsize": 8,
-        "xtick.labelsize": 8,
-        "ytick.labelsize": 8,
+        # "axes.labelsize": 8,
+        # "font.size": 8,
+        # "legend.fontsize": 8,
+        # "xtick.labelsize": 8,
+        # "ytick.labelsize": 8,
         "figure.subplot.left": 0.1,
         "figure.subplot.bottom": 0.2,
         "figure.subplot.right": 0.95,
@@ -19,6 +22,58 @@ plt.rcParams.update(
         # "backend": "macOsX"
     }
 )
+
+
+def set_size(
+    width: float
+    | int
+    | Literal["article", "ieee", "thesis", "beamer"] = 307.28987,
+    fraction=1.0,
+    subplots=(1, 1),
+):
+    """Set figure dimensions to avoid scaling in LaTeX.
+
+    Parameters
+    ----------
+    width: float or string
+            Document width in points, or string of predined document type
+    fraction: float, optional
+            Fraction of the height which you wish the figure to occupy
+    subplots: array-like, optional
+            The number of rows and columns of subplots.
+    Returns
+    -------
+    fig_dim: tuple
+            Dimensions of figure in inches
+    """
+    if width == "article":
+        width_pt = 390.0
+    elif width == "ieee":
+        width_pt = 252.0
+    elif width == "thesis":
+        width_pt = 426.79135
+    elif width == "beamer":
+        width_pt = 307.28987
+    else:
+        width_pt = width
+
+    # Width of figure (in pts)
+    fig_width_pt = width_pt
+    # Convert from pt to inches
+    inches_per_pt = 1 / 72.27
+
+    # Golden ratio to set aesthetic figure height
+    # https://disq.us/p/2940ij3
+    golden_ratio = (5**0.5 - 1) / 2
+
+    # Figure width in inches
+    fig_width_in = fig_width_pt * inches_per_pt
+    # Figure height in inches
+    fig_height_in = (
+        fig_width_in * golden_ratio * ((subplots[0] * fraction) / subplots[1])
+    )
+
+    return (fig_width_in * 1.2, fig_height_in * 1.2)
 
 
 def format_str_(str_: str) -> str:
